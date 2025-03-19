12433fd252800a2a3b3722c659c24f2b733323b7
jakubstec <kub9stec@gmail.com>
2025-03-19 14:07:00 +0100

    prepare an interaction with sqlite database, structuralize a project

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,36 @@
 import express from 'express';
+import cors from 'cors';
+import { Database } from 'sqlite3'
+import * as fs from 'fs'; // file system module
+import * as XLSX from 'xlsx'; // parsing excel files module
+
+// Load environment variables and asign a port.
+require('dotenv').config();
+const PORT = process.env.PORT || 8080;
 
 const app = express();
-const PORT = 8080;
+
+// Setup a database
+const db = new Database('db.sqlite', (error) => {
+  if(error) {
+    console.error('Error opening a database: ', error);
+  }
+  else {
+    console.log('Database connected');
+  }
+
+  // Create Table and parse Swift Codes
+
+
+});
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Test');
 })
 
-app.listen(PORT, async () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
